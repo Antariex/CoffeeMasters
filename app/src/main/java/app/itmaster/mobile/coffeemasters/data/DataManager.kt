247a8d9179ed0eeee025d561191a863fd9002860
Ariel Antequiera <arielantequiera@gmail.com>
2023-12-28 16:27:03 -0300

    complete cart funcionality

--- app/src/main/java/app/itmaster/mobile/coffeemasters/data/DataManager.kt
+++ app/src/main/java/app/itmaster/mobile/coffeemasters/data/DataManager.kt
@@ -1,13 +1,11 @@
 package app.itmaster.mobile.coffeemasters.data
 
-import android.content.ClipData.Item
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
 class DataManager : ViewModel() {
     var menu: List<Category> by mutableStateOf(listOf())
@@ -18,17 +16,14 @@ class DataManager : ViewModel() {
     }
 
     fun fetchData() {
-        // Ejecuta el getMenu en una corutina (algo así como un thread)
         viewModelScope.launch {
             menu = API.menuService.getMenu()
         }
     }
 
     fun cartAdd(product: Product) {
-        // Verifica si el producto ya está en el carrito y sumarle uno
         val existingItem = cart.find { it.product == product }
         if (existingItem != null) {
-            // El producto ya está en el carrito, incrementa la cantidad
             val updatedCart = cart.map {
                 if (it == existingItem) {
                     it.copy(quantity = it.quantity + 1)
@@ -38,25 +33,11 @@ class DataManager : ViewModel() {
             }
             cart = updatedCart
         } else {
-            // El producto no está en el carrito, agrégalo con cantidad 1
             cart = cart + ItemInCart(product, 1)
         }
-        println(cart)
     }
 
     fun cartRemove(product: Product) {
-        // Verifica si el producto está en el carrito y réstale uno
-        val existingItem = cart.find { it.product == product }
-        if (existingItem != null) {
-            // El producto está en el carrito, reduce la cantidad
-            val updatedCart = cart.map {
-                if (it == existingItem && it.quantity > 1) {
-                    it.copy(quantity = it.quantity - 1)
-                } else {
-                    it
-                }
-            }.filter { it.quantity > 0 } // Filtra los elementos con cantidad 0 o menos
-            cart = updatedCart
-        }
+        cart = cart.filter { it.product != product }
     }
 }
--- app/src/main/java/app/itmaster/mobile/coffeemasters/pages/OrderPage.kt
+++ app/src/main/java/app/itmaster/mobile/coffeemasters/pages/OrderPage.kt
@@ -1,16 +1,94 @@
 package app.itmaster.mobile.coffeemasters.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import app.itmaster.mobile.coffeemasters.data.DataManager
 
 @Composable
 fun OrderPage(dataManager: DataManager) {
-    LazyColumn {
-        items(dataManager.cart) {
-            Text("${it.quantity}x ${it.product.name}x ${it.product.price}")
+    LazyColumn(
+        modifier = Modifier
+            .padding(4.dp)
+            .background(Color(0xFFFFD3DC))
+            .fillMaxWidth()
+    ) {
+        if (dataManager.cart.isEmpty()) {
+            item {
+                Text(
+                    text = "Empty Cart",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF8B4513)
+                )
+            }
+        } else {
+            item {
+                Text(
+                    text = "ITEMS",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(4.dp),
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF8B4513)
+                )
+            }
+            items(dataManager.cart) { itemInCart ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "${itemInCart.quantity}x",
+                        textAlign = TextAlign.Start,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF8B4513),
+                        modifier = Modifier.padding(24.dp)
+                    )
+
+                    Text(
+                        text = itemInCart.product.name,
+                        textAlign = TextAlign.Start,
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Text(
+                        text = "${itemInCart.product.price}",
+                        textAlign = TextAlign.Right,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = "trash icon",
+                        tint = Color(0xFF8B4513),
+                        modifier = Modifier.clickable {
+                            dataManager.cartRemove(itemInCart.product)
+                        }
+                    )
+                }
+            }
         }
     }
 }
